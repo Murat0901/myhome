39e05899711b042df9b550286051e582321954eb
Murat0901 <menzilcim@gmail.com>
2020-04-26 14:06:51 +0300

    port add

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,13 @@ export default function Home() {
       <Layout>
         <section id="home">
           <div className="container">
-            <img src="https://avatars2.githubusercontent.com/u/47676205?s=460&u=3db26078eadd62852afdf3f952b04d238796761d&v=4"></img>
-            <h1>Kişisel Websiteme Hoş Geldiniz</h1>
-            <p>Blog sayfam için tıklayın</p>
+            <div className="row">
+              <div className="col-12 flex-column justify-content-center">
+                <img src="https://avatars2.githubusercontent.com/u/47676205?s=460&u=3db26078eadd62852afdf3f952b04d238796761d&v=4"></img>
+                <h1>Kişisel Websiteme Hoş Geldiniz</h1>
+                <p>Blog sayfam için tıklayın</p>
+              </div>
+            </div>
           </div>
         </section>
       </Layout>
